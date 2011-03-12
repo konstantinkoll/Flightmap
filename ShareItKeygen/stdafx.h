b5bda21bfe765331158140a93e662a8b1f58132f
Konstantin Koll <koll@liquidfolders.net>
2011-03-12 17:33:17 +0100

    Initial commit
    Signed-off-by: Konstantin Koll <koll@liquidfolders.net>

---
+++ ShareItKeygen/stdafx.h
@@ -0,0 +1,34 @@
+// stdafx.h : Includedatei für Standardsystem-Includedateien
+// oder häufig verwendete projektspezifische Includedateien,
+// die nur in unregelmäßigen Abständen geändert werden.
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include "License.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+#pragma warning(push, 3)
+#pragma warning(disable: 4702)
+#pragma warning(disable: 4706)
+#include "integer.h"
+#include "files.h"
+#include "osrng.h"
+#include "pssr.h"
+#include "rsa.h"
+#include "filters.h"
+#include "cryptlib.h"
+#include "sha.h"
+#include "base64.h"
+#pragma warning(pop)
+
+USING_NAMESPACE(CryptoPP)
+USING_NAMESPACE(std)
