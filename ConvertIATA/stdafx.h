497518cc344646fcb29240b9b935cdeb6d9471b1
Konstantin Koll <koll@liquidfolders.net>
2019-06-28 16:36:00 +0200

    - Stripped ConvertIATA project - Updated IATA airport database - Switched version to 2.4.11
    Signed-off-by: Konstantin Koll <koll@liquidfolders.net>

--- ConvertIATA/stdafx.h
+++ ConvertIATA/stdafx.h
@@ -16,21 +16,4 @@
 #endif
 
 #include <afx.h>
-#include <afxwin.h>         // MFC-Kern- und Standardkomponenten
-#include <afxext.h>         // MFC-Erweiterungen
-#ifndef _AFX_NO_OLE_SUPPORT
-#include <afxdtctl.h>           // MFC-Unterstützung für allgemeine Steuerelemente von Internet Explorer 4
-#endif
-#ifndef _AFX_NO_AFXCMN_SUPPORT
-#include <afxcmn.h>                     // MFC-Unterstützung für allgemeine Windows-Steuerelemente
-#endif // _AFX_NO_AFXCMN_SUPPORT
-
 #include <iostream>
-
-
-#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS      // Einige CString-Konstruktoren sind explizit.
-
-#include <atlbase.h>
-#include <atlstr.h>
-
-// TODO: Hier auf zusätzliche Header, die das Programm erfordert, verweisen.
